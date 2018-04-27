b3b1af1638c946f1ab2f16420f36131ca0fc3d52
blondgirl19 <slabinskaya@i.ua>
2018-04-27 18:20:07 +0300

    bottom navigator ready

--- app/src/main/java/ua/rogdan/trag/ui/MainActivity.java
+++ app/src/main/java/ua/rogdan/trag/ui/MainActivity.java
@@ -1,15 +1,75 @@
 package ua.rogdan.trag.ui;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationAdapter;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
+
+import java.util.IllegalFormatCodePointException;
+
+import butterknife.BindView;
 import ua.rogdan.trag.R;
+import ua.rogdan.trag.core.BaseActivity;
+import ua.rogdan.trag.core.BaseFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+    @BindView(R.id.bottom_navigation)
+    protected AHBottomNavigation bottomNavigation;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int getLayoutId() {
+        return R.layout.activity_main;
     }
+
+    @Override
+    protected void initView() {
+        initBottomMenu();
+    }
+
+    private void initBottomMenu() {
+        AHBottomNavigationAdapter navigationAdapter = new AHBottomNavigationAdapter(this, R.menu.menu_main);
+        bottomNavigation.setTitleState(AHBottomNavigation.TitleState.ALWAYS_SHOW);
+        // Change colors
+        bottomNavigation.setAccentColor(ContextCompat.getColor(this, R.color.dark_mint_green));
+        bottomNavigation.setInactiveColor(ContextCompat.getColor(this, R.color.charcoal_alpha));
+        navigationAdapter.setupWithBottomNavigation(bottomNavigation, null);
+
+        bottomNavigation.setOnTabSelectedListener(new AHBottomNavigation.OnTabSelectedListener() {
+            @Override
+            public boolean onTabSelected(int position, boolean wasSelected) {
+                if (!wasSelected) {
+                    BaseFragment fragment = null;
+
+                    switch (position) {
+                        case POSITION_HUMANS:
+
+                            break;
+                        case POSITION_TASKS:
+
+                            break;
+                        case POSITION_MAP:
+
+                            break;
+                        case POSITION_ACCOUNT:
+
+                            break;
+                    }
+
+                    if (fragment != null) {
+
+                    }
+
+                    return true;
+                }
+
+                return false;
+            }
+        });
+    }
+
+    private static final int POSITION_HUMANS = 0;
+    private static final int POSITION_TASKS = 1;
+    private static final int POSITION_MAP = 2;
+    private static final int POSITION_ACCOUNT = 3;
 }
